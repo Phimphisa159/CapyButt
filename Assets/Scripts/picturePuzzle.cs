1cd31c557e54f47af34ef837a242ff64cbe83380
Phimphisa159 <phimphisa.sris@bumail.net>
2025-02-18 21:14:15 +0700

    add click dot puzzle

--- Assets/Scripts/picturePuzzle.cs
+++ Assets/Scripts/picturePuzzle.cs
@@ -4,6 +4,8 @@ using System.Collections;
 public class picturePuzzle : MonoBehaviour
 {
     [SerializeField] GameObject image;
+    [SerializeField] GameObject text;
+    private int dot;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -13,10 +15,23 @@ public class picturePuzzle : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown("space"))
-        { 
+        if (Input.GetKeyDown("space"))
+        {
             image.SetActive(true);
-           
+
+
+            
         }
+        if (dot == 4)
+            {
+                image.SetActive(false);
+                text.SetActive(false);
+            
+            } 
+    }
+   public void setdot()
+    {
+        dot++;
+     //   Destroy(gameObject);
     }
 }
