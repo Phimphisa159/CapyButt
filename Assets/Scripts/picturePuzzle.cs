48611c39dde01070e798b92335132e3fbafe7e66
Phimphisa159 <phimphisa.sris@bumail.net>
2025-03-10 11:27:50 +0700

    add objective process UI

--- Assets/Scripts/picturePuzzle.cs
+++ Assets/Scripts/picturePuzzle.cs
@@ -1,11 +1,16 @@
 using UnityEngine;
 using System.Collections;
+using Unity.Netcode;
+using TMPro;
 
-public class picturePuzzle : MonoBehaviour
+public class picturePuzzle : NetworkBehaviour
 {
     [SerializeField] GameObject image;
     [SerializeField] GameObject text;
+    public NetworkVariable<int> TotalCoins = new NetworkVariable<int>();
     private int dot;
+    public TMP_Text healthText;
+    private bool done;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -19,17 +24,39 @@ public class picturePuzzle : MonoBehaviour
         {
             image.SetActive(true);
 
-
-            
         }
         if (dot == 4)
             {
-                image.SetActive(false);
+            if(done == true) {return; }
+            else { image.SetActive(false);
                 text.SetActive(false);
-            
-            } 
+                 TotalCoins.Value ++;
+                done = true;
+            }
+               
+
+        } 
+        healthText.text = " "+TotalCoins.Value;
     }
-   public void setdot()
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (done == true) 
+        { image.SetActive(false); }
+        else
+        {
+            if (Input.GetKeyDown("space"))
+           {
+            image.SetActive(true);
+
+           }
+        }
+       
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        image.SetActive(false);
+    }
+    public void setdot()
     {
         dot++;
      //   Destroy(gameObject);
